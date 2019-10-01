819e17ceadd534d20b3b6a7bc29b804e7cc3fd62
Michael Luby <S00188217@mail.itsligo.ie>
2019-10-01 15:53:51 +0100

    First Commit

---
+++ Ex 2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_2
+{
+    public class Car
+    {
+        private string make;
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value;  }
+        }
+
+        private string _model;
+
+        public string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+
+        private int currentSpeed;
+
+        public int CurrentSpeed
+        {
+            get { return currentSpeed; }
+            set { currentSpeed = value; }
+        }
+
+        private double engineSize;
+
+            public double EngineSize
+        {
+            get { return EngineSize; }
+            set { engineSize = value; }
+        }
+
+        public void DisplayCarInfo()
+        {
+            Console.WriteLine($"Car Make: {Make}");
+            Console.WriteLine($"Car Model: {Model}");
+            Console.WriteLine($"Current Speed: {CurrentSpeed} k/mh");
+            Console.WriteLine($"Engine Size: {EngineSize}");
+        }
+
+        public override string ToString()
+        {
+            return
+                string.Format($"Car Make: {Make}" +
+                              $"\nCar Model: {Model}" +
+                              $"\nCurrent Speed: {CurrentSpeed}" +
+                              $"\nEngine Size: {EngineSize}");
+        }
+    }
+}
--- Labsheet 3/Program.cs
+++ Labsheet 3/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using static System.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +10,21 @@ namespace Labsheet_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing");
+            Car c1 = new Car();
+            c1.Make = "Ford";
+            c1.Model = "Focus";
+            c1.CurrentSpeed = 0;
+            c1.EngineSize = 1.6;
+
+            Car c2 = new Car();
+            c2.Make = "Opel";
+            c2.Model = "Astra";
+            c2.CurrentSpeed = 0;
+            c2.EngineSize = 1.2;
+
+            Console.WriteLine($"The {c1.Make} {c1.Model}, engine size {c1.EngineSize}1 is currently travelling at >> {c1.CurrentSpeed} km/h");
+
+            Console.WriteLine($"The {c2.Make} {c2.Model}, engine size {c2.EngineSize}l is currently travelling at >> {c2.CurrentSpeed} km/h");
         }
     }
 }
